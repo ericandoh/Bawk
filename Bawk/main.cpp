4ede60ae3c80a5b50b1aeb34207c3295932dcc90
ericandoh <ohjingyu@gmail.com>
2015-09-28 12:53:29 -0700

    Initial Commit

---
+++ Bawk/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bawk
+//
+//  Created by Eric Oh on 9/28/15.
+//  Copyright (c) 2015 Eric Oh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
