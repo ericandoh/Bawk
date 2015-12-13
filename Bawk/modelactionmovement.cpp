ef0c0bfd50c54abf003957a6ccd09f6d7f8a3b99
ericandoh <ohjingyu@gmail.com>
2015-12-13 12:20:43 -0800

    added explosions behind the engine lol

--- Bawk/modelactionmovement.cpp
+++ Bawk/modelactionmovement.cpp
@@ -9,10 +9,20 @@
 #include "modelactionmovement.h"
 #include "game.h"
 #include "entity.h"
+#include "spritegetter.h"
 
 bool model_forward_callback(Game* game, Entity* owner, Entity* piece) {
     // piece->get_force() or some shizzle...and repeat for all below
     owner->move_forward(20.0f);
+    
+    fvec3 rand_offset = fvec3(rand() % 100 - 50.0f,rand() % 100 - 50.0f,rand() % 100 - 50.0f) / 400.0f;
+    fvec3 pos = piece->pos + piece->center_pos + rand_offset;
+    if (piece->parent) {
+        piece->parent->transform_into_world_coordinates_smooth(&pos, pos.x, pos.y, pos.z);
+    }
+    SpriteRender* sprite = get_sprite_instance(2, game->player);
+    sprite->set_pos(pos);
+    game->world->base_world->add_entity(sprite);
     return true;
 }
 
